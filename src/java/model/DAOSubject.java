41b7d18fae893fbb288dcb36c59055f046d73b1f
namlvhe172313 <namlvhe172313@fpt.edu.vn>
2025-08-13 21:36:25 +0700

    sua lại daosubject

--- src/java/model/DAOSubject.java
+++ src/java/model/DAOSubject.java
@@ -15,24 +15,7 @@ import java.util.List;
  * @author Admin
  */
 public class DAOSubject extends DBConnect{
-        public List<Subject> getAllSubjects() {
-        List<Subject> subjects = new ArrayList<>();
-        // Sửa đổi: Thêm cột Status vào câu lệnh SELECT
-        String sql = "SELECT SubjectID, SubjectName, Description, Status FROM Subject";
-        try (Statement st = conn.createStatement(); ResultSet rs = st.executeQuery(sql)) {
-            while (rs.next()) {
-                // Sửa đổi: Sử dụng constructor mới có status
-                subjects.add(new Subject(
-                        rs.getInt("SubjectID"),
-                        rs.getString("SubjectName"),
-                        rs.getString("Description"),
-                        rs.getString("Status")
-                ));
-            }
-        } catch (SQLException e) {
-
-
-public class DAOSubject extends DBConnect{
+        
     
     public int addSubject(Subject subject) throws SQLException{
         int result =0;
